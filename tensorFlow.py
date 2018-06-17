7f5cba209f1d0037d3fb7878ba7c3a28b3cf28b7
arjun gupta <saimzn4241@gmail.com>
2018-06-17 16:34:35 +0530

    init

---
+++ tensorFlow.py
@@ -0,0 +1,55 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import pandas as pd
+
+dataframe = pd.read_csv("dataTensorFlow.csv")
+dataframe = dataframe.drop(["index", "price", "sq_price"], axis = 1)
+dataframe = dataframe[0:10]
+print(dataframe)
+
+dataframe.loc[:, ("y1")] = [1, 1, 1, 0, 0, 1, 0, 1 , 1 , 1]
+dataframe.loc[:, ("y2")] = dataframe["y1"] == 0
+dataframe.loc[:, ("y2")] = dataframe["y2"].astype(int)
+print(dataframe)
+
+inputX = dataframe.loc[: , ['area', 'bathrooms']].as_matrix()
+inputY = dataframe.loc[: , ['y1', 'y2']].as_matrix()
+
+learning_rate = 0.000001
+training_epochs = 2000
+display_step = 50
+n_samples = inputY.size
+
+x = tf.placeholder(tf.float32, [None, 2])
+W = tf.Variable(tf.zeros([2, 2]))
+b = tf.Variable(tf.zeros([2]))
+
+y_values = tf.add(tf.matmul(x, W), b)
+y = tf.nn.softmax(y_values)
+y_ = tf.placeholder(tf.float32, [None, 2])
+
+#Cost function: Mean Squared Error
+cost = tf.reduce_sum(tf.pow(y_ - y, 2))/(2*n_samples)
+
+#Gradient Descent
+optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost)
+
+# Initialize variables and tensorflow session
+
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+for i in range(training_epochs):
+    sess.run(optimizer, feed_dict={x: inputX, y_: inputY})
+
+    if (i) % display_step == 0:
+        cc = sess.run(cost, feed_dict= {x: inputX, y_: inputY})
+        print ("Training step:", '%04d' % (i), "cost = ", "{:.9f}".format(cc))
+
+print("Optimisation Finished!")
+training_cost = sess.run(cost, feed_dict={x: inputX, y_: inputY})
+
+print("Training Cost = ", training_cost, "W=", sess.run(W), "b=", sess.run(b), '\n')
+print(sess.run(y, feed_dict={x: inputX}))
